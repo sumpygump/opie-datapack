48a039a324c3ef725a14b5a0dd3d1bb711d4c598
Jansen Price <sumpygump@gmail.com>
2024-01-02 23:20:59 -0600

    Add trident and sponge recipes and include recipe advancements for some items

--- bin/wool_matrix.py
+++ bin/wool_matrix.py
@@ -3,6 +3,7 @@
 import json
 
 TARGET_DIR = "data/opie/recipes/crafting_shapeless"
+ADVANCEMENT_DIR = "data/opie/advancements/recipes/decoration"
 
 colors = [
     "white",
@@ -25,7 +26,13 @@ colors = [
 
 
 def make_recipe(
-    namespace, full_from, full_to, input_count=1, yield_count=1, from_tag=False
+    namespace,
+    full_from,
+    full_to,
+    input_count=1,
+    yield_count=1,
+    from_tag=False,
+    group=None,
 ):
     """Make a recipe file"""
 
@@ -41,17 +48,55 @@ def make_recipe(
         ],
         "result": {"count": yield_count, "item": f"{namespace}:{full_to}"},
     }
+
+    if group:
+        recipe["group"] = group
+
     filename = f"{TARGET_DIR}/{full_to}_from_{full_from}.json"
     print(filename)
     with open(filename, "w", encoding="utf-8") as f_d:
         json.dump(recipe, f_d, indent=2)
 
 
+def make_recipe_advancement(full_from, full_to):
+    """Make the recipe advancement"""
+    obj = {
+        "parent": "minecraft:recipes/root",
+        "criteria": {
+            f"has_{full_from}": {
+                "conditions": {"items": [{"items": [f"minecraft:{full_from}"]}]},
+                "trigger": "minecraft:inventory_changed",
+            },
+            "has_the_recipe": {
+                "conditions": {
+                    "recipe": f"opie:crafting_shapeless/{full_to}_from_{full_from}"
+                },
+                "trigger": "minecraft:recipe_unlocked",
+            },
+        },
+        "requirements": [[f"has_{full_from}", "has_the_recipe"]],
+        "rewards": {"recipes": [f"opie:crafting_shapeless/{full_to}_from_{full_from}"]},
+    }
+    filename = f"{ADVANCEMENT_DIR}/{full_to}_from_{full_from}.json"
+    with open(filename, "w", encoding="utf-8") as f_d:
+        json.dump(obj, f_d, indent=2)
+
+
 def make_wool_recipe(color):
     """Make the recipe for wool from carpet"""
     make_recipe(
-        "minecraft", f"{color}_carpet", f"{color}_wool", input_count=3, yield_count=2
+        "minecraft",
+        f"{color}_carpet",
+        f"{color}_wool",
+        input_count=3,
+        yield_count=2,
+        group="wool",
     )
+
+
+def make_wool_recipe_advancement(color):
+    """Make the wool recipe advancement"""
+    make_recipe_advancement(f"{color}_carpet", f"{color}_wool")
 
 
 def make_string_recipe():
@@ -67,6 +112,7 @@ def main():
     for color in colors:
         # Three carpets of each color makes 2 wool of that color
         make_wool_recipe(color)
+        make_wool_recipe_advancement(color)
 
     # One wool of any color makes 4 string
     make_string_recipe()
